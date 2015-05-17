8d945cb528d0dff6a7be82abed0e492656c646b8
mhallin <mhallin@gmail.com>
2015-05-17 10:52:29 +0200

    Render FFT as an SVG line for improved performance

--- src/alder/node-render.cljs
+++ src/alder/node-render.cljs
@@ -1,5 +1,6 @@
 (ns alder.node-render
-  (:require [om.core :as om :include-macros true]
+  (:require [clojure.string :as string]
+            [om.core :as om :include-macros true]
             [sablono.core :as html :refer-macros [html]]
 
             [alder.node :as node]
@@ -78,60 +79,62 @@
     (render-fn input value on-change)))
 
 (defn fft-component [analyser-node owner]
-  (let [width 160
-        height 100
-        noise-floor 90
-        fft-size 256
-        data-array (js/Float32Array. (.-frequencyBinCount analyser-node))]
-    (letfn [(update-frequency-data []
-              (.getFloatFrequencyData analyser-node data-array)
-              (let [bins (.-length data-array)
-                    bin-width (/ width bins)
-                    bars (map (fn [i]
-                                (let [v (aget data-array i)
-                                      h (js/Math.round
-                                         (* height (/ (max 0 (+ noise-floor v)) noise-floor)))
-                                      x (* i bin-width)
-                                      y (- height h)]
-                                  [x y bin-width h]))
-                              (range bins))]
-                (om/set-state! owner :graph-bars bars)))
+  (let [fft-size 256]
+    (set! (.-fftSize analyser-node) fft-size)
+    (let [width 160
+          height 100
+          noise-floor 90
+          data-array (js/Float32Array. (.-frequencyBinCount analyser-node))]
+      (letfn [(update-frequency-data []
+                (.getFloatFrequencyData analyser-node data-array)
+                (let [bins (.-length data-array)
+                      bin-width (/ width bins)
+                      line-segments (map (fn [i]
+                                           (let [v (aget data-array i)
+                                                 h (js/Math.round
+                                                    (* height
+                                                       (/ (max 0 (+ noise-floor v))
+                                                          noise-floor)))
+                                                 x (* i bin-width)
+                                                 y (- height h)]
+                                            (str (if (zero? i) "M" "L")
+                                                  x "," y " ")))
+                                         (range bins))
+                      line-data (string/join line-segments)]
+                  (om/set-state! owner :line-data line-data)))
 
-            (tick-animation []
-              (update-frequency-data)
-              (when (om/get-state owner :is-mounted)
-                (.requestAnimationFrame js/window tick-animation)))]
+              (tick-animation []
+                (update-frequency-data)
+                (when (om/get-state owner :is-mounted)
+                  (.requestAnimationFrame js/window tick-animation)))]
 
-      (reify
-        om/IDisplayName
-        (display-name [_] "FFTComponent")
+        (reify
+          om/IDisplayName
+          (display-name [_] "FFTComponent")
 
-        om/IWillMount
-        (will-mount [_]
-          (set! (.-fftSize analyser-node) 256)
-          (om/set-state! owner :is-mounted true)
-          (update-frequency-data))
+          om/IWillMount
+          (will-mount [_]
+            (om/set-state! owner :is-mounted true)
+            (update-frequency-data))
 
-        om/IDidMount
-        (did-mount [_]
-          (.requestAnimationFrame js/window tick-animation))
+          om/IDidMount
+          (did-mount [_]
+            (.requestAnimationFrame js/window tick-animation))
 
-        om/IWillUnmount
-        (will-unmount [_]
-          (om/set-state! owner :s-mounted false))
+          om/IWillUnmount
+          (will-unmount [_]
+            (om/set-state! owner :is-mounted false))
 
-        om/IRender
-        (render [_]
-          (when-let [graph-bars (om/get-state owner :graph-bars)]
-            (let []
-              (html
-               [:svg.node-inspector__fft
-                {:style {:width (str width "px")
-                         :height (str height "px")}}
-                (map-indexed (fn [i [x y w h]]
-                               [:rect
-                                {:x x :y y :width w :height h :key i}])
-                             graph-bars)]))))))))
+          om/IRender
+          (render [_]
+            (when-let [line-data (om/get-state owner :line-data)]
+              (let []
+                (html
+                 [:svg.node-inspector__fft
+                  {:style {:width (str width "px")
+                           :height (str height "px")}}
+                  [:path.node-inspector__fft-line
+                   {:d line-data}]])))))))))
 
 (defn render-inspector-field [node field-type]
   (case field-type
