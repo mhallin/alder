c8400d7d9f1769e7a8da4a3347f3a20b66a17992
mhallin <mhallin@gmail.com>
2015-07-07 21:46:21 +0200

    Clojure 1.7!

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "0.0-3211"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [sablono "0.3.4"]
